f6afa2a9d0aea9c1579dd19126589c3c0530ac08
pengxin <479889183@qq.com>
2017-07-24 15:27:45 +0800

    join js

--- MyBlog/src/com/px/myblog/controller/Index.java
+++ MyBlog/src/com/px/myblog/controller/Index.java
@@ -1,21 +1,15 @@
 package com.px.myblog.controller;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-
-
 @Controller
-public class Test {
-	
-	@RequestMapping("/login.action")
-	public ModelAndView Test()
+public class Index {
+	@RequestMapping(value="index.action")
+	public ModelAndView index()
 	{
-       ModelAndView mv=new ModelAndView();
-
+		ModelAndView mv=new ModelAndView();
 		mv.setViewName("index");
 		return mv;
 	}
